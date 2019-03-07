1ae9fa1f9d82b8343913710ef64d6c986eab68d4
Rafael Batista <rafaelb24@yahoo.com>
2019-03-07 18:54:57 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,10 +9,8 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
   
-  enable :sessions
-  use Rack::Flash
-  
- #  'enable :sessions' was causing the login to loop in the browser even though all tests were passing. The session was getting cleared from one controller action to the other
+
+ #  'enable :sessions' was causing the login to loop in the browser even though all tests were passing. The session was   getting cleared from one controller action to the other
  
  # 'use Rack::Session::Cookie' fixed the log in loop and all tests still pass.
  
