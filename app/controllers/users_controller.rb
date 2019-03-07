318cf392114f7c8826eaf27e797a48eb8a95ef83
Rafael Batista <rafaelb24@yahoo.com>
2019-03-07 19:05:22 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ require './config/environment'
 class UsersController < ApplicationController
   
   get '/users/:slug' do 
+    binding.pry
     @user = User.find_by_slug(params[:slug])
     erb :'/index'
   end 
